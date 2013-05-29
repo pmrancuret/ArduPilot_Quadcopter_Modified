f009c1963b142b1a07b0f1be39edf5beefd68719
Paul Rancuret <pmrancuret@gmail.com>
2013-05-29 06:48:32 -0400

    Added Eclipse Project Files and updated README

---
+++ Release/events.d
@@ -0,0 +1,94 @@
+events.d events.o: ../events.pde ../ArduPilot_2_7_Modified.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\arduino\cores\arduino/Arduino.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/stdlib.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/string.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/math.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/pgmspace.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/inttypes.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/stdint.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/io.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/sfr_defs.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/iom328p.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/portpins.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/common.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/version.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/fuse.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/lock.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/interrupt.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\arduino\cores\arduino/binary.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\arduino\cores\arduino/WCharacter.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/ctype.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\arduino\cores\arduino/WString.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\arduino\cores\arduino/HardwareSerial.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\arduino\cores\arduino/Stream.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\arduino\cores\arduino/Print.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/stdio.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\arduino\cores\arduino/Printable.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\arduino\cores\arduino/new.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\arduino\variants\standard/pins_arduino.h \
+  ../AP_Config.h ../AP_PID_settings.h ../defines.h \
+  C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/eeprom.h
+
+../ArduPilot_2_7_Modified.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\arduino\cores\arduino/Arduino.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/stdlib.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/string.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/math.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/pgmspace.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/inttypes.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/stdint.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/io.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/sfr_defs.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/iom328p.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/portpins.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/common.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/version.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/fuse.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/lock.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/interrupt.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\arduino\cores\arduino/binary.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\arduino\cores\arduino/WCharacter.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/ctype.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\arduino\cores\arduino/WString.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\arduino\cores\arduino/HardwareSerial.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\arduino\cores\arduino/Stream.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\arduino\cores\arduino/Print.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/stdio.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\arduino\cores\arduino/Printable.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\arduino\cores\arduino/new.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\arduino\variants\standard/pins_arduino.h:
+
+../AP_Config.h:
+
+../AP_PID_settings.h:
+
+../defines.h:
+
+C:\arduino-1.0.4-windows\arduino-1.0.4\hardware\tools\avr\avr\include/avr/eeprom.h:
