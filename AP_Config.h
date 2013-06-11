c3603f8478a520b69255e70951a5e5986d9e711d
John McArthur <johnnyfisma@gmail.com>
2013-06-11 07:27:45 -0400

    Checking to see if I can use the shell version
    made a small comment

--- AP_Config.h
+++ AP_Config.h
@@ -1,6 +1,7 @@
 /* ArduPilot 2.7.1 Header file : July 23, 2010 */
 // Testing comments
 // Can you see me? Testing GIT
+// Can I use git push master origin (or origin master)?
 /*
 These Flight Modes can be changed either here or directly in events.pde
 	
